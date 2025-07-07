e17bc73b9bbf9bc1c133f9036fe0046c6c30e47e
Md Jamshed Uddin <jamsheduddin03@gmail.com>
2025-07-07 12:33:45 +0600

    search bar and filter and set values to url

--- src/app/agents/page.tsx
+++ src/app/agents/page.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { Suspense } from "react";
 import agentsData from "../../../public/mock-agents.json";
 import { AgentType } from "@/lib/types";
 import AgentCard from "@/components/AgentCard";
+import FilterAndSearch from "@/components/FilterAndSearch";
+
 const agents = agentsData as AgentType[];
 
-const AgentsPage = async () => {
-  console.log(agents);
+const AgentsPage = async ({
+  searchParams,
+}: {
+  searchParams?: Promise<{
+    status: string;
+    category: string;
+    priceModel: string;
+  }>;
+}) => {
+  const params = await searchParams;
+
+  console.log(params);
 
   return (
     <div>
+      <div className="mb-8">
+        <Suspense>
+          <FilterAndSearch />
+        </Suspense>
+      </div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
         {agents.map((agent) => (
           <AgentCard key={agent.id} agent={agent} />
---
+++ src/components/FilterAndSearch.tsx
@@ -0,0 +1,151 @@
+"use client";
+
+import React, { ChangeEvent } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { useRouter, useSearchParams } from "next/navigation";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { useDebouncedCallback } from "use-debounce";
+
+const FilterAndSearch = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const onFilterChange = (name: string, value: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set(name, value);
+
+    if (!value.trim()) {
+      params.delete(name);
+    } else {
+      params.set(name, value);
+    }
+
+    router.replace(`/agents?${params.toString()}`);
+    console.log(name, value);
+  };
+
+  const onSearchBarChange = useDebouncedCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const params = new URLSearchParams(searchParams);
+      const { value } = e.target;
+      console.log(value);
+
+      if (!value.trim()) {
+        params.delete("search");
+      } else {
+        params.set("search", value.trim());
+      }
+
+      router.replace(`/agents?${params.toString()}`);
+    },
+    600
+  );
+
+  const clearFilter = () => {
+    const params = new URLSearchParams(searchParams);
+
+    params.delete("status");
+    params.delete("category");
+    params.delete("priceMode");
+
+    router.replace(`/agents?${params.toString}`);
+  };
+
+  return (
+    <div className="flex flex-col gap-3 lg:gap-0 lg:flex-row items-center justify-between">
+      <div className="flex w-full max-w-sm items-center gap-2">
+        <Input
+          type="text"
+          placeholder="Search agent"
+          className="rounded-xl text-sm"
+          onChange={onSearchBarChange}
+        />
+        <Button
+          type="submit"
+          variant="outline"
+          size="sm"
+          className="rounded-xl "
+        >
+          Search
+        </Button>
+      </div>
+
+      <div className="flex items-center gap-3">
+        <Select
+          name="status"
+          onValueChange={(value) => onFilterChange("status", value)}
+          value={searchParams.get("status") || " "}
+        >
+          <SelectTrigger className="w-fit rounded-xl">
+            <SelectValue placeholder="Status" />
+          </SelectTrigger>
+          <SelectContent className="bg-black rounded-xl">
+            <SelectGroup>
+              <SelectItem value=" ">Status</SelectItem>
+              <SelectItem value="Active">Active</SelectItem>
+              <SelectItem value="Beta">Beta</SelectItem>
+              <SelectItem value="Archived">Archived</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Select
+          name="category"
+          onValueChange={(value) => onFilterChange("category", value)}
+          value={searchParams.get("category") || " "}
+        >
+          <SelectTrigger className="w-fit rounded-xl">
+            <SelectValue placeholder="Category" />
+          </SelectTrigger>
+          <SelectContent className="bg-black rounded-xl">
+            <SelectGroup>
+              <SelectItem value=" ">Category</SelectItem>
+              <SelectItem value="Customer Service">Customer Service</SelectItem>
+              <SelectItem value="Operations">Operations</SelectItem>
+              <SelectItem value="Marketing">Marketing</SelectItem>
+              <SelectItem value="Data Analysis">Data Analysis</SelectItem>
+              <SelectItem value="Development">Development</SelectItem>
+              <SelectItem value="Human Resources">Human Resources</SelectItem>
+              <SelectItem value="Finance">Finance</SelectItem>
+              <SelectItem value="Legal">Legal</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Select
+          name="priceModel"
+          onValueChange={(value) => onFilterChange("priceModel", value)}
+          value={searchParams.get("priceModel") || " "}
+        >
+          <SelectTrigger className="w-fit rounded-xl">
+            <SelectValue placeholder="Price model" />
+          </SelectTrigger>
+          <SelectContent className="bg-black rounded-xl">
+            <SelectGroup>
+              <SelectItem value=" ">Price model</SelectItem>
+              <SelectItem value="Free-Tier">Free-Tier</SelectItem>
+              <SelectItem value="Subscription">Subscription</SelectItem>
+              <SelectItem value="Per-Use">Per-Use</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Button
+          onClick={clearFilter}
+          variant="outline"
+          className="rounded-xl cursor-pointer"
+        >
+          Clear <XMarkIcon className="w-4 h-4" />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default FilterAndSearch;
